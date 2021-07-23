086ef7edb02f2c8b39eafe88638e7a13536a8e8b
Kunal Tyagi <kunaltyagi93@gmail.com>
2021-07-23 18:22:40 +0530

    added crud app

---
+++ crud-app/src/Component/AllUsers.jsx
@@ -0,0 +1,75 @@
+import react, { useState, useEffect } from 'react';
+import { Table, TableHead, TableCell, Paper, TableRow, TableBody, Button, makeStyles } from '@material-ui/core'
+import { getUsers, deleteUser } from '../Service/api';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles({
+    table: {
+        width: '90%',
+        margin: '50px 0 0 50px'
+    },
+    thead: {
+        '& > *': {
+            fontSize: 20,
+            background: '#000000',
+            color: '#FFFFFF'
+        }
+    },
+    row: {
+        '& > *': {
+            fontSize: 18
+        }
+    }
+})
+
+
+const AllUsers = () => {
+    const [users, setUsers] = useState([]);
+    const classes = useStyles();
+
+    useEffect(() => {
+        getAllUsers();
+    }, []);
+
+    const deleteUserData = async (id) => {
+        await deleteUser(id);
+        getAllUsers();
+    }
+
+    const getAllUsers = async () => {
+        let response = await getUsers();
+        setUsers(response.data);
+    }
+
+    return (
+        <Table className={classes.table}>
+            <TableHead>
+                <TableRow className={classes.thead}>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Username</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Phone</TableCell>
+                    <TableCell></TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {users.map((user) => (
+                    <TableRow className={classes.row} key={user._id}>
+                        <TableCell>{user._id}</TableCell>
+                        <TableCell>{user.name}</TableCell>
+                        <TableCell>{user.username}</TableCell>
+                        <TableCell>{user.email}</TableCell>
+                        <TableCell>{user.phone}</TableCell>
+                        <TableCell>
+                            <Button color="primary" variant="contained" style={{marginRight:10}} component={Link} to={`/edit/${user._id}`}>Edit</Button>
+                            <Button color="secondary" variant="contained" onClick={() => deleteUserData(user._id)}>Delete</Button> 
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
+
+export default AllUsers;
