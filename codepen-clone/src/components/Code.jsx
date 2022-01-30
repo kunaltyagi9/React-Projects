665b62fb03225a1ca440accc5bc4741e47323da1
Kunal Tyagi <kunaltyagi93@gmail.com>
2022-01-30 23:58:39 +0530

    created codepen clone with react

---
+++ codepen-clone/src/components/Code.jsx
@@ -0,0 +1,52 @@
+import { useContext } from 'react';
+
+//components
+import Editor from './Editor';
+
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles'
+import { Code as CodeIcon } from '@mui/icons-material';
+
+import { DataContext } from '../context/DataProvider';
+
+const Container = styled(Box)`
+    background-color: #060606;
+    height: 50vh;
+    display: flex;
+`
+
+const Code = () => {
+
+    const { html, css, js, setHtml, setCss, setJs } = useContext(DataContext);
+
+    return (
+        <Container>
+            <Editor 
+                language="xml"
+                heading="HTML"
+                value={html}
+                onChange={setHtml}
+                icon='/'
+                color='#FF3C41'
+            />
+            <Editor 
+                language="css"
+                heading="CSS"
+                value={css}
+                onChange={setCss}
+                icon='*'
+                color='#0EBEFF'
+            />
+            <Editor 
+                language="javascript"
+                heading="JS"
+                value={js}
+                onChange={setJs}
+                icon='( )'
+                color='#FCD000'
+            />
+        </Container>
+    )
+}
+
+export default Code;
---
+++ codepen-clone/src/components/Result.jsx
@@ -0,0 +1,47 @@
+import { useState, useEffect, useContext } from 'react';
+
+import { DataContext } from '../context/DataProvider';
+
+import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const Container = styled(Box)`
+    height: 41vh;
+`
+
+const Result = () => {
+
+    const [src, setSrc] = useState('');
+    const { html, css, js } = useContext(DataContext);
+
+    const srcCode = `
+        <html>
+            <body>${html}</body>
+            <style>${css}</style>
+            <script>${js}</script>
+        </html>
+    `
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrc(srcCode);
+        }, 250);
+
+        return () => clearTimeout(timeout);
+    }, [html, css, js])
+
+    return (
+        <Container style={html || css || js ? null : {background: '#444857' }}>
+            <iframe 
+                srcDoc={src}
+                title="output"
+                sandbox="allow-scrips"
+                frameBorder="0"
+                width="100%"
+                height="100%"
+            />    
+        </Container>
+    )
+}
+
+export default Result;
