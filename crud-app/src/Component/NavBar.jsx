9f19b88261c1ea8e9fd22f405542863608b3723d
Jaydip <114307569+DeepDevJay@users.noreply.github.com>
2023-07-29 13:22:23 +0530

    Update NavBar.jsx
    it is giving the console error for the exact path that's why I have changed it and it works.

--- crud-app/src/Component/NavBar.jsx
+++ crud-app/src/Component/NavBar.jsx
@@ -17,9 +17,9 @@ const NavBar = () => {
     return (
         <Header position="static">
             <Toolbar>
-                <Tabs to="./" exact>Code for Interview</Tabs>
-                <Tabs to="all" exact>All Users</Tabs>
-                <Tabs to="add" exact>Add User</Tabs>
+                <Tabs to="./" exact="true">Code for Interview</Tabs>
+                <Tabs to="all" exact="true">All Users</Tabs>
+                <Tabs to="add" exact="true">Add User</Tabs>
             </Toolbar>
         </Header>
     )
