16ad76634b377996bb2eb1ef8d01b6d068e0360c
Jaydip <114307569+DeepDevJay@users.noreply.github.com>
2023-07-29 13:25:16 +0530

    Update NotFound.jsx
    it is giving console error for alternate text instead of image.

--- crud-app/src/Component/NotFound.jsx
+++ crud-app/src/Component/NotFound.jsx
@@ -2,7 +2,7 @@ import notfound from '../Assets/Images/NotFound.jpg';
 
 const NotFound = () => {
     return (
-        <img src={notfound}  style={{width: '30%', margin: '80px 0 0 35%'}}/>
+        <img src={notfound} alt="image not found" style={{width: '30%', margin: '80px 0 0 35%'}}/>
     )
 }
 
